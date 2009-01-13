fd797593e7d47269d918b4acf6820570d29db7c0
joe.google@psychlaw.com.au <joedj@users.noreply.github.com>
2009-01-13 08:24:50 +0000

    Retry with smaller net buffer size to support unpatched libogc, at the cost of performance.

--- source/common.c
+++ source/common.c
@@ -43,7 +43,7 @@ misrepresented as being the original software.
 
 #include "common.h"
 
-#define NET_BUFFER_SIZE 32768
+#define MAX_NET_BUFFER_SIZE 32768
 #define FREAD_BUFFER_SIZE 32768
 
 VIRTUAL_PARTITION VIRTUAL_PARTITIONS[] = {
@@ -70,6 +70,7 @@ VIRTUAL_PARTITION *PA_NAND  = VIRTUAL_PARTITIONS + 7;
 VIRTUAL_PARTITION *PA_ISFS  = VIRTUAL_PARTITIONS + 7;
 
 static const u32 CACHE_PAGES = 8;
+static u32 NET_BUFFER_SIZE = MAX_NET_BUFFER_SIZE;
 
 static bool fatInitState = false;
 static bool _dvd_mountWait = false;
@@ -439,13 +440,19 @@ typedef s32 (*transferrer_type)(s32 s, void *mem, s32 len);
 static s32 transfer_exact(s32 s, char *buf, s32 length, transferrer_type transferrer) {
     s32 result = 0;
     s32 remaining = length;
+    s32 bytes_transferred;
     set_blocking(s, true);
     while (remaining) {
-        s32 bytes_transferred = transferrer(s, buf, MIN(remaining, NET_BUFFER_SIZE));
+        try_again_with_smaller_buffer:
+        bytes_transferred = transferrer(s, buf, MIN(remaining, NET_BUFFER_SIZE));
         if (bytes_transferred > 0) {
             remaining -= bytes_transferred;
             buf += bytes_transferred;
         } else if (bytes_transferred < 0) {
+            if (bytes_transferred == -EINVAL && NET_BUFFER_SIZE == MAX_NET_BUFFER_SIZE) {
+                NET_BUFFER_SIZE = 4096;
+                goto try_again_with_smaller_buffer;
+            }
             result = bytes_transferred;
             break;
         } else {
@@ -490,8 +497,13 @@ s32 recv_to_file(s32 s, FILE *f) {
     char buf[NET_BUFFER_SIZE];
     s32 bytes_read;
     while (1) {
+        try_again_with_smaller_buffer:
         bytes_read = net_read(s, buf, NET_BUFFER_SIZE);
         if (bytes_read < 0) {
+            if (bytes_read == -EINVAL && NET_BUFFER_SIZE == MAX_NET_BUFFER_SIZE) {
+                NET_BUFFER_SIZE = 4096;
+                goto try_again_with_smaller_buffer;
+            }
             // if (bytes_read != -EAGAIN) {
             //     printf("DEBUG: recv_to_file() net_read error: [%i] %s\n", -bytes_read, strerror(-bytes_read));
             // }
