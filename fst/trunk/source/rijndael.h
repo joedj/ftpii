6e578a7b4dfaa3db3160a8073ce08079fcf768b9
joe.google@psychlaw.com.au <joe.google@psychlaw.com.au@da8fd9da-f04f-0410-853f-e538ee1d4d2c>
2009-01-15 14:02:44 +0000

    Fix compiler warnings in this 3rd-party code

--- fst/trunk/source/rijndael.h
+++ fst/trunk/source/rijndael.h
@@ -1,13 +1,17 @@
-/* Rijndael Block Cipher - rijndael.c
+/* 
+    Rijndael Block Cipher - rijndael.c
 
-   Written by Mike Scott 21st April 1999
-   mike@compapp.dcu.ie
+    Written by Mike Scott 21st April 1999
+    mike@compapp.dcu.ie
 
-   Permission for free direct or derivative use is granted subject 
-   to compliance with any conditions that the originators of the 
-   algorithm place on its exploitation.  
-
+    Permission for free direct or derivative use is granted subject 
+    to compliance with any conditions that the originators of the 
+    algorithm place on its exploitation.  
 */
+#ifndef _RIJNDAEL_H
+#define _RIJNDAEL_H
 
 void aes_set_key(u8 *key);
 void aes_decrypt(u8 *iv, u8 *inbuf, u8 *outbuf, unsigned long long len);
+
+#endif /* _RIJNDAEL_H */
