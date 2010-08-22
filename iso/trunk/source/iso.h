2236ba30c1d7615c267e3b7269426f3d5d9b3194
joe.google@psychlaw.com.au <joe.google@psychlaw.com.au@da8fd9da-f04f-0410-853f-e538ee1d4d2c>
2010-08-22 07:38:08 +0000

    Change license to GPLv2

--- iso/trunk/source/iso.h
+++ iso/trunk/source/iso.h
@@ -1,28 +1,6 @@
-/*
-
-libiso -- an ISO9660 DVD devoptab library for the Wii
-
-Copyright (C) 2008 Joseph Jordan <joe.ftpii@psychlaw.com.au>
-
-This software is provided 'as-is', without any express or implied warranty.
-In no event will the authors be held liable for any damages arising from
-the use of this software.
-
-Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribute it
-freely, subject to the following restrictions:
-
-1.The origin of this software must not be misrepresented; you must not
-claim that you wrote the original software. If you use this software in a
-product, an acknowledgment in the product documentation would be
-appreciated but is not required.
-
-2.Altered source versions must be plainly marked as such, and must not be
-misrepresented as being the original software.
-
-3.This notice may not be removed or altered from any source distribution.
-
-*/
+// Copyright 2010 Joseph Jordan <joe.ftpii@psychlaw.com.au>
+// This code is licensed to you under the terms of the GNU GPL, version 2;
+// see file COPYING or http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 #ifndef _ISO_H
 #define _ISO_H
 
